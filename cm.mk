eb64bf928237e8e1f29eb302962d52b8f49341c1
ryanwheat85 <bklkaboss@gmail.com>
2016-11-07 05:50:51 -0600

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -3,11 +3,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
-$(call inherit-product-if-exists, vendor/samsung/j2lte/j2lte-vendor.mk)
+$(call inherit-product-if-exists, vendor/samsung/j1xlteatt/j1xlteatt-vendor.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/samsung/j2lte/overlay
+DEVICE_PACKAGE_OVERLAYS += device/samsung/j1xlteatt/overlay
 
-LOCAL_PATH := device/samsung/j2lte
+LOCAL_PATH := device/samsung/j1xteatt
 ifeq ($(TARGET_PREBUILT_KERNEL),)
 	LOCAL_KERNEL := $(LOCAL_PATH)/kernAl
 else
@@ -20,7 +20,7 @@ PRODUCT_COPY_FILES += \
 
 $(call inherit-product, build/target/product/full.mk)
 
-PRODUCT_NAME := cm_j2lte
-PRODUCT_DEVICE := j2lte
+PRODUCT_NAME := j1xlteuc
+PRODUCT_DEVICE := j1xlteatt
 PRODUCT_BRAND := Samsung
-PRODUCT_MODEL := j2lte
+PRODUCT_MODEL := j1xlteatt
