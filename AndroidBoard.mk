beafd10b8dad5b4262af4a2968fd415fdfcdc3f9
ryanwheat85 <bklkaboss@gmail.com>
2016-11-07 05:34:12 -0600

    Update AndroidBoard.mk

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -5,4 +5,4 @@ include $(CLEAR_VARS)
 ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
 # include the non-open-source counterpart to this file
--include vendor/samsung/j2lte/AndroidBoardVendor.mk
+-include vendor/samsung/j1xlteatt/AndroidBoardVendor.mk
